95bc2dd86b2734a5d4b7b37fd81b34aca61d0f19
vladislav2 <etter.id75@gmail.com>
2021-01-27 17:39:08 +0200

    Initial Commit

---
+++ VeretennikovVE_TrafficLights_HW2.1.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VeretennikovVE_TrafficLights_HW2.1.2
+//
+//  Created by user on 27.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
