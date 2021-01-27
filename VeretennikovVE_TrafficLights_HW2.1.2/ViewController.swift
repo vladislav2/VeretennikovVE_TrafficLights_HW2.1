4c3265102f8bd5ba01f46f3d6dc3e63e9d59c1b5
vladislav2 <etter.id75@gmail.com>
2021-01-27 22:02:03 +0200

    add constraints and write logic traffic lights

--- VeretennikovVE_TrafficLights_HW2.1.2/ViewController.swift
+++ VeretennikovVE_TrafficLights_HW2.1.2/ViewController.swift
@@ -8,12 +8,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+  var isStartButtonPressed = false
+  
+  let redColorWithAlpha = UIColor.red.withAlphaComponent(0.1)
+  let yellowColorWithAlpha = UIColor.yellow.withAlphaComponent(0.1)
+  let greenColorWithAlpha = UIColor.green.withAlphaComponent(0.1)
+  
+  @IBOutlet weak var redView: UIView!
+  @IBOutlet weak var yellowView: UIView!
+  @IBOutlet weak var greenView: UIView!
+  @IBOutlet weak var startButton: UIButton!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    redView.backgroundColor = redColorWithAlpha
+    yellowView.backgroundColor = yellowColorWithAlpha
+    greenView.backgroundColor = greenColorWithAlpha
+    
+    startButton.layer.cornerRadius = 20
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    
+    redView.layer.cornerRadius = redView.frame.height / 2
+    yellowView.layer.cornerRadius = yellowView.bounds.height / 2
+    greenView.layer.cornerRadius = greenView.bounds.height / 2
   }
 
-
+  @IBAction func startButtonPressed() {
+    if redView.backgroundColor == UIColor.red {
+      yellowView.backgroundColor = .yellow
+      redView.backgroundColor = redColorWithAlpha
+    } else if yellowView.backgroundColor == UIColor.yellow {
+      greenView.backgroundColor = .green
+      yellowView.backgroundColor = yellowColorWithAlpha
+    } else if greenView.backgroundColor == UIColor.green{
+      redView.backgroundColor = .red
+      greenView.backgroundColor = greenColorWithAlpha
+    }
+      
+    if isStartButtonPressed == false {
+      isStartButtonPressed.toggle()
+      startButton.setTitle("NEXT", for: .normal)
+      redView.backgroundColor = .red
+    }
+    
+    
+  }
+  
 }
 
